04fa258084cc87bb41a2ffe7fbcfb9a1fe533169
Michael Giesler <michael.giesler1@icloud.com>
2024-12-04 16:37:37 -0700

    worked out house page feature to include both components asyncronyously(however that word is spelled)

---
+++ src/pages/HousesPage.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { housesService } from '../services/HousesService';
+import Pop from '../utils/Pop';
+import { AppState } from '../AppState';
+import { computed } from 'vue';
+
+const houses = computed(() => AppState.houses)
+const account = computed(() => AppState.account)
+const showForm = ref(false)
+
+onMounted(async () => {
+  try {
+    await housesService.getHouses()
+  } catch (error) {
+    Pop.error(error)
+  }
+})
+</script>
+
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12">
+        <div class="d-flex justify-content-between align-items-center">
+          <h1>Houses🏘️</h1>
+          <button v-if="account" @click="showForm = !showForm" class="btn btn-success">
+            {{  showForm ? 'Close Form' : 'List A House' }}
+          </button>
+        </div>
+      </div>
+    </section>
+
+    <section v-if="showForm" class="row justify-content-center mb-4">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <HouseForm />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="row g-4">
+      <div v-for="house in houses" :key="house-id" class="col-md-4 mb-3">
+        <HouseCard :houseProp="house" />
+      </div>
+    </section>
+  </div>
+</template>
